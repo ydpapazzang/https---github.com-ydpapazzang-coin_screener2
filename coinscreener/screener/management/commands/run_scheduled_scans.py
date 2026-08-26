b15c6224a7c88082accefc9d2cc182849ba11e48
ydpapa_gogo <i.am.psw86@gmail.com>
2026-08-26 11:27:15 +0200

    P1 operations: add coinscreener/screener/management/commands/run_scheduled_scans.py

---
+++ coinscreener/screener/management/commands/run_scheduled_scans.py
@@ -0,0 +1,22 @@
+import json
+
+from django.core.management.base import BaseCommand
+
+from coinscreener.screener.scheduled_scans import run_scheduled_scans
+
+
+class Command(BaseCommand):
+    help = '현재 KST 슬롯의 예약 전략을 Gunicorn과 분리된 프로세스에서 실행합니다.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--force', action='store_true')
+
+    def handle(self, *args, **options):
+        result = run_scheduled_scans(
+            force=options['force'],
+            output=self.stdout.write,
+        )
+        self.stdout.write(json.dumps(result, ensure_ascii=False, default=str))
+        if not result['ok']:
+            self.stderr.write(self.style.WARNING('일부 예약 스캔이 실패했습니다.'))
+
