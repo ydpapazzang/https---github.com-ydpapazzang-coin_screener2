479cc0c39ca70d538da8ddfdc1ca6982fac3f984
ydpapa_gogo <i.am.psw86@gmail.com>
2026-09-05 11:33:50 +0200

    feat: track and show intraday danta observations

---
+++ coinscreener/screener/migrations/0033_intradayobservation_tracking_prices.py
@@ -0,0 +1,19 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [('screener', '0032_intradayobservation')]
+
+    operations = [
+        migrations.AddField(
+            model_name='intradayobservation',
+            name='highest_price',
+            field=models.FloatField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name='intradayobservation',
+            name='lowest_price',
+            field=models.FloatField(blank=True, null=True),
+        ),
+    ]
+
